6e592ceb3b0781c801d138c969a97d44e4e09cdc
Madhawakavindu <madhawakavindu7@gmail.com>
2026-01-09 22:04:38 +0530

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:moviewave/pages/home_page.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -13,6 +16,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "MovieWave",
       debugShowCheckedModeBanner: false,
+      //app eke theme eka set karaganna puluwan mehema, e kiyanne dark theme ekak use karanna
+      theme: ThemeData(
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        brightness: Brightness.dark,
+      ),
       home: HomePage(),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home), 
+            label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.play_circle_fill),
+            label: "Now Playing",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie_creation_outlined),
+            label: "TV Shows",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search), 
+            label: "Search"),
+        ],
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.redAccent,
+      ),
+    );
   }
 }
