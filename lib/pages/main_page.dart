910a65c66f5ac68ed016189479a8684c1ea7d54c
madhawa <madhawakavindu7@gmail.com>
2026-01-15 01:37:43 +0530

    u

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:moviewave/models/movie_mode.dart';
+import 'package:moviewave/service/movies_service.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -8,8 +10,86 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  List<Movie> _movies = [];
+  int _currentPage = 1;
+  bool _isLoading = false;
+  bool _hasMore = true;
+
+  //the method to fetch the upcomming movies from the API and this method is called in the initState method.
+
+  Future<void> _fetchMovies() async {
+    if (_isLoading || !_hasMore) {
+      return;
+    }
+    setState(() {
+      //loading state ekak pennanna oni e nisa
+      _isLoading = true;
+    });
+    // eka digata scroll karanawa nam user kenek podi delayed time ekk denw
+    await Future.delayed(const Duration(seconds: 1));
+    // dan data tika fetch krgamu
+    try {
+      final newMovies = await MoviesService().fetchUpcommingMovies(
+        page: _currentPage,
+      );
+      print(newMovies.length);
+      setState(() {
+        if (newMovies.isEmpty) {
+          _hasMore = false;
+        } else {
+          _movies.addAll(newMovies);
+          _currentPage++;
+        }
+      });
+    } catch (error) {
+      print("Error:$error");
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchMovies();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("MovieWave")));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "MovieWave",
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.redAccent,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification notification) {
+          if (!_isLoading &&
+              notification.metrics.pixels ==
+                  notification.metrics.maxScrollExtent) {
+            _fetchMovies();
+          }
+          return false;
+        },
+
+        child: ListView.builder(
+          itemCount: _movies.length + (_isLoading ? 1 : 0),
+          itemBuilder: (context, index) {
+            if (index == _movies.length) {
+              return const Center(child: CircularProgressIndicator());
+              ;
+            }
+            return ListTile(title: Text(_movies[index].title.toString()));
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/service/movies_service.dart
+++ lib/service/movies_service.dart
@@ -1,8 +1,35 @@
 //api requests ekak liyanawa
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:moviewave/models/movie_mode.dart';
 
 class MoviesService {
   //get the api key from .env
   final String _apikey = dotenv.env["TMDB_DATABASE_KEY"] ?? "";
+
+  final String _baseUrl = "https://api.themoviedb.org/3/movie/upcoming";
+
+  //fetch all upcomming movies(function eka)
+  Future<List<Movie>> fetchUpcommingMovies({int page = 1}) async {
+    try {
+      final responce = await http.get(
+        Uri.parse("$_baseUrl?api_key=$_apikey&page=$page"),
+      );
+      // responce ek hriyt awd kiyl check krgnn
+      if (responce.statusCode == 200) {
+        final data = json.decode(responce.body);
+        final List<dynamic> results = data["results"];
+
+        return results.map((movieData) => Movie.fromJson(movieData)).toList();
+      } else {
+        throw Exception("Faild to load the data");
+      }
+    } catch (error) {
+      print("Error fetching upcomming movies $error");
+      return [];
+    }
+  }
 }
